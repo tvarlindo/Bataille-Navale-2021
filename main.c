adf08b8b1b6024f74d4714c5948066588204ab6a
tvarlindo <56110351+tvarlindo@users.noreply.github.com>
2021-03-03 10:35:35 +0100

    ajout grille

--- main.c
+++ main.c
@@ -1,7 +1,34 @@
+/*
+ * \Bataille Navale
+ * \Autheur Arlindo Tavares Varela
+ * \version 0.1
+ * \date 02.03.2021
+ */
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    printf("bonjour");
+#define TAILLEGRILLE 10
+#define GRILLEAFFICHAGE
+
+{\
+{"", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"},\
+{"A","","","","","","","","","",""},\
+{"B","","","","","","","","","",""},\
+{"C","","","","","","","","","",""},\
+{"D","","","","","","","","","",""},\
+{"E","","","","","","","","","",""},\
+{"F","","","","","","","","","",""},\
+{"G","","","","","","","","","",""},\
+{"H","","","","","","","","","",""},\
+{"I","","","","","","","","","",""},\
+{"J","","","","","","","","","",""},\
+}
+
+char *table_grille_affichage[TAILLEGRILLE+1][TAILLEGRILLE+1] = GRILLEAFFICHAGE;
+
+
+int main(){
+    printf("%s", GRILLEAFFICHAGE);
+
+
     return 0;
 }
